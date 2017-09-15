dcf0e6170a334990251691c17dd27e8f496916d3
Matías Martínez <soy@matmartinez.net>
2017-09-15 18:46:59 -0300

    Added workaround for safe area insets.

---
+++ Classes/MMSnapController+MMSafeAreaInsetsWorkaround.h
@@ -0,0 +1,13 @@
+//
+//  MMSnapController+MMSafeAreaInsetsWorkaround.h
+//  MMSnapController
+//
+//  Created by Matías Martínez on 9/15/17.
+//  Copyright © 2017 Matías Martínez. All rights reserved.
+//
+
+#import "MMSnapController.h"
+
+@interface MMSnapController (MMSafeAreaInsetsWorkaround)
+
+@end
---
+++ Classes/MMSnapController+MMSafeAreaInsetsWorkaround.m
@@ -0,0 +1,43 @@
+//
+//  MMSnapController+MMSafeAreaInsetsWorkaround.m
+//  MMSnapController
+//
+//  Created by Matías Martínez on 9/15/17.
+//  Copyright © 2017 Matías Martínez. All rights reserved.
+//
+
+#import "MMSnapController+MMSafeAreaInsetsWorkaround.h"
+#import <objc/runtime.h>
+
+@implementation MMSnapController (MMSafeAreaInsetsWorkaround)
+
++ (void)load
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        const BOOL requiresWorkaround = ([UIView instancesRespondToSelector:@selector(safeAreaInsets)]);
+        if (requiresWorkaround) {
+            NSString *overrideSelectorString = [@[ @"_edgeI", @"nsetsForChildView", @"Controller:insetsAreA", @"bsolute:" ] componentsJoinedByString:@""];
+            SEL overrideSelector = NSSelectorFromString(overrideSelectorString);
+            
+            Method workaroundMethod = class_getInstanceMethod(self, @selector(MM_safeAreaInsetsForChildViewController:insetsAreAbsolute:));
+            IMP workaroundImplementation = method_getImplementation(workaroundMethod);
+            
+            class_addMethod(self, overrideSelector, workaroundImplementation, method_getTypeEncoding(workaroundMethod));
+        }
+    });
+}
+
+- (UIEdgeInsets)MM_safeAreaInsetsForChildViewController:(UIViewController *)childViewController insetsAreAbsolute:(BOOL *)absolute
+{
+    if (@available(iOS 11.0, *)) {
+        if (absolute) {
+            *absolute = YES;
+        }
+        return self.view.safeAreaInsets;
+    } else {
+        return UIEdgeInsetsZero;
+    }
+}
+
+@end
